4382bf6e8cb416b960fc2bce7849d415d96f8265
abdullahalomair57-cmd <abdullahalomair57@gmail.com>
2026-05-11 00:47:49 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ provider "google" {
 }
 
 resource "google_compute_network" "main_vpc" {
-  name                    = "main-vpc"
+  name                    = "abdullah-vpc"
   auto_create_subnetworks = false
 }
